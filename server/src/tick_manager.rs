1655530cdc26bc1a165f26ec7b644b5941b1b136
Charles Bournhonesque <cbournhonesque@snapchat.com>
2023-11-10 14:59:14 -0500

    copy similar to what naia tick are doing

--- server/src/tick_manager.rs
+++ server/src/tick_manager.rs
@@ -3,6 +3,9 @@ use lightyear_shared::{TickConfig, TimeManager, WrappedTime};
 
 pub struct TickManager {
     config: TickConfig,
+    /// Number of the fixed-update schedule run. (incremented by one every time we run the fixed update schedule once.
+    /// (can be incremented 0, 1, multiple times during a single render frame)
+    physics_tick: Tick,
     /// current tick
     tick: Tick,
     /// time when we switched to the current tick
@@ -14,9 +17,14 @@ impl TickManager {
     pub fn from_config(config: TickConfig) -> Self {
         Self {
             config,
+            physics_tick: Tick(0),
             tick: Tick(0),
             last_tick_wrapped_time: WrappedTime::new(0),
         }
+    }
+
+    pub fn increment_physics_tick(&mut self) {
+        self.physics_tick += 1
     }
 
     /// Update the status of the TickManager after time advances by `elapsed`
@@ -26,7 +34,7 @@ impl TickManager {
         if time_offset > self.config.tick_duration {
             // TODO: compute the actual tick duration
             self.tick += 1;
-            self.last_tick_wrapped_time = time_manager.current_time();
+            self.last_tick_wrapped_time = *time_manager.current_time();
             return true;
         }
         return false;
