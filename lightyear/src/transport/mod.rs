ea5681db4885eb93217ee24b73a5811a224e864d
Charles Bournhonesque <cbournhonesque@snapchat.com>
2023-12-31 05:07:19 -0500

    wip

--- lightyear/src/transport/mod.rs
+++ lightyear/src/transport/mod.rs
@@ -15,6 +15,7 @@ pub(crate) mod udp;
 /// The transport is a map of channels (used for server, during testing)
 pub(crate) mod channels;
 
+mod steam;
 /// The transport is using WebTransport
 #[cfg(feature = "webtransport")]
 pub mod webtransport;
