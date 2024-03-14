b064c107a37c670a97dca61143ce161c064d3dad
Charles Bournhonesque <cbournhonesque@snapchat.com>
2024-03-14 11:45:19 -0400

    fix

--- examples/simple_box/src/server.rs
+++ examples/simple_box/src/server.rs
@@ -61,8 +61,8 @@ pub(crate) struct Global {
     pub client_id_to_entity_id: HashMap<ClientId, Entity>,
 }
 
-pub(crate) fn init(mut commands: Commands, connections: ResMut<ServerConnections>) {
-    for mut connection in connections.servers {
+pub(crate) fn init(mut commands: Commands, mut connections: ResMut<ServerConnections>) {
+    for connection in &mut connections.servers {
         let _ = connection.start().inspect_err(|e| {
             error!("Failed to start server: {:?}", e);
         });
