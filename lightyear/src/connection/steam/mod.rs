1fa71b345b91ef5c6b72df2a1e13cd6b2947f0e9
Tim Irmler <zwazel@hotmail.com>
2024-03-15 09:37:19 +0100

    steam works

--- lightyear/src/connection/steam/mod.rs
+++ lightyear/src/connection/steam/mod.rs
@@ -1,2 +1,9 @@
+use std::sync::{Arc, RwLock};
+
 pub(crate) mod client;
 pub(crate) mod server;
+
+pub(crate) struct SingleClientThreadSafe(Arc<RwLock<steamworks::SingleClient>>);
+
+unsafe impl Sync for SingleClientThreadSafe {}
+unsafe impl Send for SingleClientThreadSafe {}
