1575751e9cd18f56a1e66ce25060f3c4cf08f9a1
cBournhonesque <charlesbour@gmail.com>
2024-04-14 16:19:18 -0400

    wip

--- lightyear/src/connection/steam/mod.rs
+++ lightyear/src/connection/steam/mod.rs
@@ -19,10 +19,11 @@ pub(crate) fn get_networking_options(
         0,
     )];
     if let Some(ref conditioner) = conditioner {
-        options.push(NetworkingConfigEntry::new_float(
-            NetworkingConfigValue::FakePacketLossRecv,
-            conditioner.incoming_loss * 100.0,
-        ));
+        // TODO: float options are not useable, see https://github.com/Noxime/steamworks-rs/pull/168
+        // options.push(NetworkingConfigEntry::new_float(
+        //     NetworkingConfigValue::FakePacketLossRecv,
+        //     conditioner.incoming_loss * 100.0,
+        // ));
         options.push(NetworkingConfigEntry::new_int32(
             NetworkingConfigValue::FakePacketLagRecv,
             conditioner.incoming_latency.as_millis() as i32,
