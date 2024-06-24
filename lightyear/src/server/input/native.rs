1abfadab9e814dc2e5a4a0611ec1189b3b4ef31c
cBournhonesque <charlesbour@gmail.com>
2024-06-24 10:54:48 -0400

    fix

--- lightyear/src/server/input/native.rs
+++ lightyear/src/server/input/native.rs
@@ -4,6 +4,7 @@ use bevy::utils::HashMap;
 
 use crate::inputs::native::input_buffer::InputBuffer;
 use crate::inputs::native::InputMessage;
+use crate::prelude::server::DisconnectEvent;
 use crate::prelude::{is_started, ClientId, MessageRegistry, TickManager, UserAction};
 use crate::protocol::message::MessageKind;
 use crate::serialize::reader::Reader;
@@ -12,23 +13,6 @@ use crate::server::events::InputEvent;
 use crate::shared::replication::network_target::NetworkTarget;
 use crate::shared::sets::{InternalMainSet, ServerMarker};
 
-// - ClientInputs:
-// - inputs will be sent via a special message
-// - in each packet, we will send the inputs for the last 10-15 frames. Can use the ring buffer?
-// - First: send all inputs for last 15 frames. Along with the tick at which the input was sent
-// - Maybe opt: Only send the inputs that have changed and the tick at which they change?
-// - in the client: we don't send a packet every tick. So what we do is:
-// - during fixedupdate, we store the input for the given tick. Store that input in a ringbuffer containing the input history. (for at least the rollback period)
-// - at the end of the frame, we collect the last 15 ticks of inputs and put them in a packet.
-// - we send that packet via tick-buffered sender, associated with the last client tick
-// - IS THIS CORRECT APPROACH? IT WOULD MEAN THAT WE WOULD READ THAT PACKET ONLY ON THE CURRENT TICK IN THE SERVER, BUT ACTUALLY WE WANT TO READ IT IMMEDIATELY
-// (BECAUSE IT CONTAINS LAST 15 TICKS OF INPUTS, SO CAN HELP FILL GAPS IN INPUTS!)
-// - IT WOULD SEEM THAT WE CAN JUST SEND THE PACKET AS SEQUENCED-UNRELIABLE. (WE DONT NEED TO KNOW THE PACKET TICK BECAUSE IT CONTAINS TICKS)
-// ON THE SERVER WE READ IMMEDIATELY AND WE UPDATE OUR RINGBUFFER OF INPUTS THAT WE CAN FETCH FROM!
-// - during rollback, we can read from the input history
-// - the input history is associated with a connection.
-// - in the server, we receive the inputs, open the packet, and update the entire ringbuffer of inputs?
-// - server is at tick 9. for example we didn't receive the input for tick 10,11; but we receive the packet for tick 12, which contains all the inputs for ticks 10,11,12.
 pub struct InputPlugin<A> {
     _marker: std::marker::PhantomData<A>,
 }
@@ -100,6 +84,17 @@ impl<A: UserAction> Plugin for InputPlugin<A> {
             FixedPostUpdate,
             clear_input_events::<A>.in_set(InputSystemSet::ClearInputEvents),
         );
+        app.add_systems(Last, handle_client_disconnect::<A>);
+    }
+}
+
+/// Remove the client if the client disconnects
+fn handle_client_disconnect<A: UserAction>(
+    mut events: EventReader<DisconnectEvent>,
+    mut input_buffers: ResMut<InputBuffers<A>>,
+) {
+    for event in events.read() {
+        input_buffers.buffers.remove(&event.client_id);
     }
 }
 
