a72c0c7bab99f31e0386fb2c58f61a5169fd4568
Sirmadeira <nicolaumori@gmail.com>
2024-10-16 08:45:55 -0300

    Oops adjusted trace wrong

--- examples/simple_box/src/server.rs
+++ examples/simple_box/src/server.rs
@@ -126,7 +126,7 @@ fn movement(
     for input in input_reader.read() {
         let client_id = input.context();
         if let Some(input) = input.input() {
-            info!(
+            trace!(
                 "Receiving input: {:?} from client: {:?} on tick: {:?}",
                 input,
                 client_id,
