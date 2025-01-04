81fdb838b1cfa997bf4c1e53129349e27382fa01
cBournhonesque <charlesbour@gmail.com>
2025-01-04 16:51:53 -0500

    make sure ParentSync is synced to predicted/interpolated entities

--- lightyear/src/client/prediction/spawn.rs
+++ lightyear/src/client/prediction/spawn.rs
@@ -84,3 +84,95 @@ pub(crate) fn spawn_predicted_entity(
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::client::components::Confirmed;
+    use crate::prelude::server::SyncTarget;
+    use crate::prelude::{client, server, ClientId, NetworkTarget};
+    use crate::tests::stepper::{BevyStepper, TEST_CLIENT_ID};
+    use bevy::hierarchy::{BuildChildren, Parent};
+    use bevy::prelude::default;
+
+    /// https://github.com/cBournhonesque/lightyear/issues/627
+    /// Test that when we spawn a parent + child with hierarchy (ParentSync),
+    /// the parent-child hierarchy is maintained on the predicted entities
+    #[test]
+    fn test_spawn_predicted_with_hierarchy() {
+        let mut stepper = BevyStepper::default();
+
+        let c = ClientId::Netcode(TEST_CLIENT_ID);
+        let server_child = stepper.server_app.world_mut().spawn_empty().id();
+        let server_parent = stepper
+            .server_app
+            .world_mut()
+            .spawn(server::Replicate {
+                sync: SyncTarget {
+                    prediction: NetworkTarget::All,
+                    ..default()
+                },
+                ..default()
+            })
+            .add_child(server_child)
+            .id();
+
+        stepper.frame_step();
+        stepper.frame_step();
+        stepper.frame_step();
+
+        // check that the parent and child are spawned on the client
+        let confirmed_child = stepper
+            .client_app
+            .world()
+            .resource::<client::ConnectionManager>()
+            .replication_receiver
+            .remote_entity_map
+            .get_local(server_child)
+            .expect("child entity was not replicated to client");
+        let confirmed_parent = stepper
+            .client_app
+            .world()
+            .resource::<client::ConnectionManager>()
+            .replication_receiver
+            .remote_entity_map
+            .get_local(server_parent)
+            .expect("parent entity was not replicated to client");
+
+        // check that the parent-child hierarchy is maintained
+        assert_eq!(
+            stepper
+                .client_app
+                .world()
+                .get::<Parent>(confirmed_child)
+                .expect("confirmed child entity doesn't have a parent")
+                .get(),
+            confirmed_parent
+        );
+
+        let predicted_child = stepper
+            .client_app
+            .world()
+            .get::<Confirmed>(confirmed_child)
+            .unwrap()
+            .predicted
+            .expect("confirmed child entity doesn't have a predicted entity");
+        let predicted_parent = stepper
+            .client_app
+            .world()
+            .get::<Confirmed>(confirmed_parent)
+            .unwrap()
+            .predicted
+            .expect("confirmed parent entity doesn't have a predicted entity");
+
+        // check that the parent-child hierarchy is present on the predicted entities
+        assert_eq!(
+            stepper
+                .client_app
+                .world()
+                .get::<Parent>(predicted_child)
+                .expect("predicted child entity doesn't have a parent")
+                .get(),
+            predicted_parent
+        );
+    }
+}
