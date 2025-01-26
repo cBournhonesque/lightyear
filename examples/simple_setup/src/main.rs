3134f136485493c2560f9bf6ea2501937d7ac95a
cBournhonesque <charlesbour@gmail.com>
2025-01-26 09:23:07 -0500

    fix simple setup example

--- examples/simple_setup/src/main.rs
+++ examples/simple_setup/src/main.rs
@@ -7,20 +7,38 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-#[cfg(feature = "client")]
 mod client;
-#[cfg(feature = "server")]
 mod server;
 mod shared;
 
 use bevy::prelude::*;
+use clap::{Parser, Subcommand, ValueEnum};
+
+/// CLI options to create an [`App`]
+#[derive(Parser, Debug)]
+#[command(version, about)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub mode: Mode,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Mode {
+    Client,
+    Server
+}
 
 fn main() {
+    let cli = Cli::parse();
     let mut app = App::new();
-    #[cfg(feature = "client")]
-    app.add_plugins(client::ExampleClientPlugin);
-    #[cfg(feature = "server")]
-    app.add_plugins(server::ExampleServerPlugin);
-
+    
+    match cli.mode {
+        Mode::Client => {
+            app.add_plugins(client::ExampleClientPlugin);
+        }
+        Mode::Server => {
+            app.add_plugins(server::ExampleServerPlugin);
+        }
+    }
     app.run();
 }
