34dae6f0c1f785aabcbecdafcb82263e151bc5ec
Charles Bournhonesque <charlesbour@gmail.com>
2025-05-09 17:53:07 +0900

    make AppTriggerExt pub

--- lightyear/src/lib.rs
+++ lightyear/src/lib.rs
@@ -207,6 +207,7 @@ pub mod prelude {
     pub use crate::protocol::message::{
         registry::{AppMessageExt, MessageRegistry},
         resource::AppResourceExt,
+        trigger::AppTriggerExt
     };
     pub use crate::protocol::serialize::AppSerializeExt;
     pub use crate::shared::config::SharedConfig;
