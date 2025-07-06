0ea1d96b02aa10d1dd06d33fc43a063d0dd573e9
elizabeth-suehr <77520680+elizabeth-suehr@users.noreply.github.com>
2025-07-05 21:33:51 -0700

    Temp Test

--- lightyear_udp/src/server.rs
+++ lightyear_udp/src/server.rs
@@ -84,7 +84,7 @@ impl ServerUdpPlugin {
             let socket = std::net::UdpSocket::bind(local_addr)?;
             socket.set_nonblocking(true)?;
             udp_io.socket = Some(socket);
-            commands.entity(trigger.target()).insert((Linked, UdpLinkOfIO));
+            commands.entity(trigger.target()).insert(Linked);
         }
         Ok(())
     }
@@ -98,7 +98,7 @@ impl ServerUdpPlugin {
 
     fn send(
         mut server_query: Query<(&mut ServerUdpIo, &Server), With<Linked>>,
-        mut link_query: Query<(&mut Link, &PeerAddr), With<UdpLinkOfIO>>,
+        mut link_query: Query<(&mut Link, &PeerAddr)>,
     ) {
         // TODO: parallelize
         server_query
