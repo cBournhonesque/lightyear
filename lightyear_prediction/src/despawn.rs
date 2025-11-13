e9eecf734ea49d54d9d402f585182c1af44a1980
Sirmadeira <nicolaumori@gmail.com>
2025-11-13 14:29:31 -0300

    Feat: Downgrade log (#1300)

--- lightyear_prediction/src/despawn.rs
+++ lightyear_prediction/src/despawn.rs
@@ -52,7 +52,7 @@ impl Command for PredictionDespawnCommand {
                 // if this is a predicted entity, do not despawn the entity immediately but instead
                 // add a PredictionDisable component to it to mark it as disabled until the confirmed
                 // entity catches up to it
-                info!(?self.entity, "inserting prediction disable marker");
+                debug!(?self.entity, "inserting prediction disable marker");
                 entity.insert(PredictionDisable);
             } else {
                 error!("This command should only be called for predicted entities!");
