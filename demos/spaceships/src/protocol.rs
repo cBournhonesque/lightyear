93cce58cc5ae5c703fa1f42924621f5c86ce093a
Charles Bournhonesque <charlesbour@gmail.com>
2026-07-06 20:57:14 -0400

    fix demo

--- demos/spaceships/src/protocol.rs
+++ demos/spaceships/src/protocol.rs
@@ -207,14 +207,14 @@ impl Plugin for ProtocolPlugin {
             .predict()
             .with_rollback_condition(position_should_rollback)
             .add_linear_interpolation()
-            .add_linear_correction_fn();
+            .add_correction();
 
         app.component::<Rotation>()
             .replicate()
             .predict()
             .with_rollback_condition(rotation_should_rollback)
             .add_linear_interpolation()
-            .add_linear_correction_fn();
+            .add_correction();
     }
 }
 
