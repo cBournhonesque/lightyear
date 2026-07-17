693fd56b4f40599271539d855bea25020da03603
Periwink <charlesbour@gmail.com>
2026-07-17 03:15:43 -0400

    fix avian2d (#1599)

--- examples/avian_2d/src/shared.rs
+++ examples/avian_2d/src/shared.rs
@@ -55,3 +55,105 @@ impl Plugin for SharedPlugin {
         app.add_systems(Startup, init);
 
         // physics
+        app.add_plugins(lightyear::avian2d::plugin::LightyearAvianPlugin {
+            replication_mode: AvianReplicationMode::Position {
+                sync_to_transform: false,
+            },
+            ..default()
+        });
+        app.add_plugins(
+            PhysicsPlugins::default()
+                .build()
+                // disable the position<>transform sync plugins as it is handled by lightyear_avian
+                .disable::<PhysicsTransformPlugin>()
+                .disable::<PhysicsInterpolationPlugin>(),
+        )
+        .insert_resource(Gravity(Vec2::ZERO));
+
+        crate::debug::register_debug_systems(app);
+    }
+}
+
+pub(crate) fn init(mut commands: Commands) {
+    commands.spawn(WallBundle::new(
+        Vec2::new(-WALL_SIZE, -WALL_SIZE),
+        Vec2::new(-WALL_SIZE, WALL_SIZE),
+        Color::WHITE,
+    ));
+    commands.spawn(WallBundle::new(
+        Vec2::new(-WALL_SIZE, WALL_SIZE),
+        Vec2::new(WALL_SIZE, WALL_SIZE),
+        Color::WHITE,
+    ));
+    commands.spawn(WallBundle::new(
+        Vec2::new(WALL_SIZE, WALL_SIZE),
+        Vec2::new(WALL_SIZE, -WALL_SIZE),
+        Color::WHITE,
+    ));
+    commands.spawn(WallBundle::new(
+        Vec2::new(WALL_SIZE, -WALL_SIZE),
+        Vec2::new(-WALL_SIZE, -WALL_SIZE),
+        Color::WHITE,
+    ));
+}
+
+// Generates a pseudo-random color from the peer id.
+pub(crate) fn color_from_id(client_id: PeerId) -> Color {
+    let h = (((client_id.to_bits().wrapping_mul(30)) % 360) as f32) / 360.0;
+    let s = 1.0;
+    let l = 0.5;
+    Color::hsl(h, s, l)
+}
+
+// Applies movement input to a player velocity.
+pub(crate) fn shared_movement_behaviour(
+    mut velocity: Mut<LinearVelocity>,
+    action: &ActionState<PlayerActions>,
+) {
+    trace!(pressed = ?action.get_pressed(), "shared movement");
+    const MOVE_SPEED: f32 = 10.0;
+    if action.pressed(&PlayerActions::Up) {
+        velocity.y += MOVE_SPEED;
+    }
+    if action.pressed(&PlayerActions::Down) {
+        velocity.y -= MOVE_SPEED;
+    }
+    if action.pressed(&PlayerActions::Left) {
+        velocity.x -= MOVE_SPEED;
+    }
+    if action.pressed(&PlayerActions::Right) {
+        velocity.x += MOVE_SPEED;
+    }
+    *velocity = LinearVelocity(velocity.clamp_length_max(MAX_VELOCITY));
+}
+
+// Wall
+#[derive(Bundle)]
+pub(crate) struct WallBundle {
+    color: ColorComponent,
+    physics: PhysicsBundle,
+    wall: Wall,
+    name: Name,
+}
+
+#[derive(Component)]
+pub(crate) struct Wall {
+    pub(crate) start: Vec2,
+    pub(crate) end: Vec2,
+}
+
+impl WallBundle {
+    pub(crate) fn new(start: Vec2, end: Vec2, color: Color) -> Self {
+        Self {
+            color: ColorComponent(color),
+            physics: PhysicsBundle {
+                collider: Collider::segment(start, end),
+                collider_density: ColliderDensity(1.0),
+                rigid_body: RigidBody::Static,
+                restitution: Restitution::new(0.0),
+            },
+            wall: Wall { start, end },
+            name: Name::from("Wall"),
+        }
+    }
+}
